ada098da1ad8f5514ada23d296d43a4e1f7799ff
緒方哲也 <>
2021-09-13 20:32:16 +0900

    ♻️ 状態を変更するときにprevをつけて名前をわかりやすくした。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,7 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if(count<10) {
-      setCount( count => count +1);
+      setCount( prevCount => prevCount +1);
     }
   }, [count]);
 
@@ -28,7 +28,7 @@ export default function Home() {
   }, [])
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow );
+    setIsShow((prevIsShow) => !prevIsShow );
   }, []);
 
   useEffect( () => {
